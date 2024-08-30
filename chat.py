f62c86f1e87f01344694272a5700ef218812cb23
Firthunand <fdo.delabarra@gmail.com>
2024-08-30 16:41:09 -0400

    elimacion de index.html

--- chat.py
+++ chat.py
@@ -1,14 +1,11 @@
-from flask import Flask, render_template, request, redirect, url_for, session
+import openai
 import mysql.connector
 import pandas as pd
-import openai
-
-app = Flask(__name__)
 
 # Configura tu clave de API de OpenAI
 openai.api_key = ''
 
-# Conexión a la base de datos
+#Conexión a la base de datos
 my_conn = mysql.connector.connect(
         host='',
         user='',
@@ -36,20 +33,11 @@ def interaccion_Gpt(pregunta, respuestas):
     )
     return respuesta.choices[0].message["content"]
 
-## formulario preguntas
-@app.route('/', methods=['POST','GET'])
-def formulario():
-    
-    respuestas_abiertas = df['recomendacion_abierta'].dropna().tolist()
-    respuestas_formateadas = "\n".join([f"- {resp}" for resp in respuestas_abiertas])
-    
-    respuesta=None
-    if request.method == "POST":
-        pregunta=request.form['pregunta']
-        
-        respuesta = interaccion_Gpt(pregunta,respuestas_formateadas)        
-    
-    return render_template('formulario.html', respuesta=respuesta)
-    
-    
+respuestas_abiertas = df['recomendacion_abierta'].dropna().tolist()
+respuestas_formateadas = "\n".join([f"- {resp}" for resp in respuestas_abiertas])    
+respuesta=None
+pregunta = input("¿Cual es tu pregunta? : ") 
+respuesta = interaccion_Gpt(pregunta,respuestas_formateadas)
+print(respuesta)
+
 
